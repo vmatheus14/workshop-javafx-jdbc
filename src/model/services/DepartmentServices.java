fa44a9cf3d07a35c3b7973c07930096c46133708
Matheus <vmbdl@icloud.comgit add .git config --global user email vmbdl@icloud.com$ git config --global user.email vmbdl@icloud.com>
2022-08-05 21:08:28 -0300

    DepartmentList controller and more

---
+++ src/model/services/DepartmentServices.java
@@ -0,0 +1,15 @@
+package model.services;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentServices {
+	
+	
+	
+	public List<Department> findAll() {
+		return null;
+	}
+
+}
